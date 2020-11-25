e458098de4177c3471fb26c32f6dfd8524fd68ea
Ronan Drost <ronandrost20@gmail.com>
2020-11-25 22:00:46 +0100

    jhjhahhbajuhujkhin,nhujk i,kuiljhn

--- public/js/index.js
+++ public/js/index.js
@@ -1,28 +1,28 @@
-function PageTransition(){
+
+function pageTransition() {
 
     var tl = gsap.timeline();
 
     tl.to('ul.transition li', { duration: 0.5, scaleY: 1, transformOrigin: "bottom left", stagger: 0.2 });
-    tl.to('ul.transition.li', { duration: 0.5, scaleY: 0, transformOrigin: "bottom left", stagger: 0.1, delay: 0.1 });
+    tl.to('ul.transition li', { duration: 0.5, scaleY: 0, transformOrigin: "bottom left", stagger: 0.1, delay: 0.1 });
 }
 
 function contentAnimation() {
 
     var tl = gsap.timeline();
     tl.from('.left', { duration: 1.5, translateY: 50, opacity: 0 });
-    tl.to('img', { clipPath: "polygon(0 0, 100% 100%, 0% 100%" }, "-=1.1");
+    tl.to('img', { clipPath: "polygon(0 0, 100% 0, 100% 100%, 0% 100%)" }, "-=0.5");
 
 }
 
 function delay(n) {
     n = n || 2000;
-    return new Promise(done => {
+    return new Promise(resolve => {
         setTimeout(() => {
             done();
         }, n);
     });
 }
-
 
 barba.init({
 
@@ -31,10 +31,10 @@ barba.init({
     transitions: [{
 
         async leave(data) {
-            
+
             const done = this.async();
 
-            PageTransition();
+            pageTransition();
             await delay(1500);
             done();
         },
@@ -48,3 +48,14 @@ barba.init({
         }
     }]
 });
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function () {
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+        document.getElementById("nav").style.top = "0";
+    } else {
+        document.getElementById("nav").style.top = "-50px";
+    }
+    prevScrollpos = currentScrollPos;
+};
