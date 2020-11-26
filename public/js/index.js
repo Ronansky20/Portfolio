2ee9dfbc3a75466ab9c1d9aa70234a67e508a9e8
Ronan Drost <ronandrost20@gmail.com>
2020-11-26 13:18:30 +0100

    Fix?

--- public/js/index.js
+++ public/js/index.js
@@ -76,6 +76,7 @@ document.addEventListener('DOMContentLoaded',function(event){
     // chekc if text isn't finished yet
     if (i < (text.length)) {
       // add next character to h1
+     // @ts-ignore
      document.querySelector("h1").innerHTML = text.substring(0, i+1) +'<span aria-hidden="true"></span>';
 
       // wait for a while and call this function again for next character
